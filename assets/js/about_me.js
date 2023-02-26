1e0208f72195711553429c73c8cd214dff78ad92
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-02-26 19:01:54 +0200

    Update about_me.js

--- assets/js/about_me.js
+++ assets/js/about_me.js
@@ -1,46 +1,8 @@
 const about_me = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  phoneNumber: "",
-  location: "",
-  professionalSummary: "",
+  header:"About me",
+  professional_summary: "A highly organized and creative professional with 5+ years of experience in customer service and project management. Offering strong communication, problem-solving and analytical skills and a history of success developing impactful interactions between humans and computers. Focused on creating empowering experiences through user-centric design.",
+  linkedin: "https://www.linkedin.com/in/dlozimthethwa/",
+  
 
-  setFirstName: function(name) {
-    this.firstName = name;
-  },const resume = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  phoneNumber: "",
-  location: "",
-  professionalSummary: "",
 
-  setFirstName: function(name) {
-    this.firstName = name;
-  },
-
-  setLastName: function(name) {
-    this.lastName = name;
-  },
-
-  setEmail: function(email) {
-    this.email = email;
-  },
-
-  setPhoneNumber: function(phoneNumber) {
-    this.phoneNumber = phoneNumber;
-  },
-
-  setLocation: function(location) {
-    this.location = location;
-  },
-
-  setProfessionalSummary: function(summary) {
-    this.professionalSummary = summary;
-  },
-
-  getFullName: function() {
-    return this.firstName + " " + this.lastName;
-  }
 };
