c3a7c080ae4b2f43e7e4dbfe8cb5ba023c859696
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-02-27 18:34:30 +0200

    Update about_me.js

--- assets/js/about_me.js
+++ assets/js/about_me.js
@@ -2,7 +2,26 @@ const about_me = {
   header:"About me",
   professional_summary: "A highly organized and creative professional with 5+ years of experience in customer service and project management. Offering strong communication, problem-solving and analytical skills and a history of success developing impactful interactions between humans and computers. Focused on creating empowering experiences through user-centric design.",
   linkedin: "https://www.linkedin.com/in/dlozimthethwa/",
-  
+  const education = [
+  [
+      {
+        type: "Certificate",
+        field: "HIV Testing and Counselling",
+        institution: "HWSETA",
+        location: "Johannesburg, Gauteng",
+        date: "February 2017 to February 2018"
+      }
+    ],
+    [
+      {
+        type: "High School",
+        level: "National Senior Certificate (NSC)",
+        institution: "Vryheid High School",
+        location: "Vryheid, KwaZulu-Natal",
+        date: "January 2004 to December 2008"
+      }
+    ]
+  ],
 
   function build_structure() {
     // Create the main tag
