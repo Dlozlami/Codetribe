a9b4cbf09df0f2b1ea958cc13951b27db72c15ea
UX DLOZI <40693444+Dlozlami@users.noreply.github.com>
2023-02-28 23:28:24 +0200

    Update about_me.js

--- assets/js/about_me.js
+++ assets/js/about_me.js
@@ -1,53 +1,35 @@
+// Create the main tag
+const mainElement = document.createElement("main");
+
+// Create the article tag
+const articleElement = document.createElement("article");
+
+// Add the article tag to the main tag
+mainElement.appendChild(articleElement);
+
+// Add the main tag to the document body
+document.body.appendChild(mainElement);
+
 const about_me = {
   
   header:"About me",
   professional_summary: "A highly organized and creative professional with 5+ years of experience in customer service and project management. Offering strong communication, problem-solving and analytical skills and a history of success developing impactful interactions between humans and computers. Focused on creating empowering experiences through user-centric design.",
   linkedin: "https://www.linkedin.com/in/dlozimthethwa/",
-  education : [
-    [
-      {
-        type: "Certificate",
-        field: "HIV Testing and Counselling",
-        institution: "HWSETA",
-        location: "Johannesburg, Gauteng",
-        date: "February 2017 to February 2018"
-      }
-    ],
-    [
-      {
-        type: "High School",
-        level: "National Senior Certificate (NSC)",
-        institution: "Vryheid High School",
-        location: "Vryheid, KwaZulu-Natal",
-        date: "January 2004 to December 2008"
-      }
-    ]
-  ],
-
-  build_structure: function() {
-    // Create the main tag
-    const mainElement = document.createElement("main");
-
-    // Create the article tag
-    const articleElement = document.createElement("article");
-
-    // Add the article tag to the main tag
-    mainElement.appendChild(articleElement);
-
-    // Add the main tag to the document body
-    document.body.appendChild(mainElement);
-  },
-
+  education : [["Certificate in HIV Testing and Counselling HW/SP/150791", "HWSETA - Johannesburg, Gauteng", "February 2017 to February 2018"], ["High School (matric) in National Senior Certificate (NSC)", "Vryheid High School - Vryheid, KwaZulu-Natal", "January 2004 to December 2008"]],
 
   display_heading: function() {
   const html = `
-    <section>
       <h1>About me</h1>
-      <p>${professional_summary}</p>
-      <a href="${linkedin}" style="text-decoration: none;color: black;"><i class="bi bi-linkedin">${linkedin}</i></a>
-      <hr>
-    </section>`;
-  articleTag.appendChild(html);
+      <p>${about_me.professional_summary}</p>
+      <a href="${about_me.linkedin}" style="text-decoration: none;color: black;"><i class="bi bi-linkedin">${about_me.linkedin}</i></a>
+      <hr>`;
+
+  let mainTag = document.getElementsByTagName("main")[0];
+  let articleTag = mainTag.getElementsByTagName("article")[0];
+  let frame = document.createElement("section")
+  frame.innerHTML = html;
+  articleTag.appendChild(frame);
+
   },
 
   display_education: function() {
@@ -63,9 +45,10 @@ const about_me = {
   let educationHR = document.createElement("hr");
   educationSection.appendChild(educationHR);
 
-  let educationArray = education;
+  let educationArray = about_me.education;
   for (let i = 0; i < educationArray.length; i++) {
     let educationItem = educationArray[i];
+    
     
     let jobTitle = document.createElement("span");
     jobTitle.classList.add("job_title");
@@ -76,29 +59,31 @@ const about_me = {
     employer.classList.add("employer");
     employer.textContent = educationItem[1];
     educationSection.appendChild(employer);
-
+    
     let duration = document.createElement("span");
     duration.classList.add("duration");
     duration.textContent = educationItem[2];
     educationSection.appendChild(duration);
-
-    let br = document.createElement("br");
-    educationSection.appendChild(br);
-    let br2 = document.createElement("br");
-    educationSection.appendChild(br2);
+    let p = document.createElement("p");
+    educationSection.appendChild(p);
   }
 
-  let mainTag = document.getElementsByTagName("main")[0];
-  let articleTag = mainTag.getElementsByTagName("article")[0];
-  articleTag.appendChild(educationSection);
+  articleElement.appendChild(educationSection);
+  let p = document.createElement("p");
+  articleElement.appendChild(p);
   }
 
 };
 
 function main() {
-  
+  // Build the basic structure of the page
+  // about_me.build_structure();
 
+  // Display the heading and professional summary
+  about_me.display_heading();
 
+  // Display the education section
+  about_me.display_education();
 }
 
 // Call the main function when the page finishes loading
